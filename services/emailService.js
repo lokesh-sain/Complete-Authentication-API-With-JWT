4496a167fc35b734ced4028005ec4adef0bd9319
lokeshsain <68300683+lokesh-sain@users.noreply.github.com>
2023-10-04 12:44:05 +0530

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -3,13 +3,10 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: true,
+    secure: false,
     auth: {
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASSWORD,
-    },
-    tls: {
-        ciphers:'SSLv3'
     }
   });
 
